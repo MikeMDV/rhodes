f26958f1462953ec371e53427357770494ad40bd
Michael Marven <mmarven09@gmail.com>
2016-02-19 10:29:05 -0500

    Adding advising appointment project component

---
+++ AdvisingApptEmailComponent/email_test.py
@@ -0,0 +1,38 @@
+#
+#             Author: Michael Marven
+#       Date Created: 02/17/16
+# Last Date Modified: 02/18/16
+#          File Name: email_test.py
+#           Overview: A script to test the appt_email module classes 
+#
+#                     Note: The Gmail account must have "Allow less secure apps"
+#                     set to ON. Go to Gmail>Settings>Accounts and Import>
+#                     Other Google Account settings>Connected apps & sites>
+#                     Allow less secure apps
+#
+#
+#
+
+from appt_email import EmailMsg
+
+# Declare variables
+from_addr = "FROM EMAIL ADDRESS"
+to_addr = "TO EMAIL ADDRESS"
+server = 'smtp.gmail.com'
+server_port = 587
+email_pwd = "FROM EMAIL PASSWORD" # TODO: Move this to config.py module
+
+email_subj = "Test of new Python script for sending email"
+email_body = "This is a new test"
+
+# Create EmailMsg object
+
+test1 = EmailMsg(from_addr, to_addr, server, server_port, email_pwd)
+
+# Send email
+
+test1.sendEmail(email_subj, email_body)
+
+# Print confirmation
+
+print "Email sucesfully sent."
